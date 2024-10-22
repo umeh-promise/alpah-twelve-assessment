8071ea6f86edc695651047bb0de31dc3c1bc7274
Umeh Promise <umehpromise1000@gmail.com>
2024-10-22 22:54:17 +0100

    feat: Add DashboardOverviewCard component

---
+++ app/(dashboard)/_components/dashboard-overview-card.tsx
@@ -0,0 +1,34 @@
+import { ArrowIcon, InfoIcon } from "@/assets/icons";
+import { cn } from "@/utils";
+
+export default function DashboardOverviewCard({
+  title,
+  value,
+  percentage,
+}: {
+  title: string;
+  value: string;
+  percentage: number;
+}) {
+  const percentageValue = percentage < 1 ? "down" : "up";
+
+  return (
+    <article className="rounded-[2px] border border-grey-400 p-6">
+      <h4 className="flex items-center gap-1 font-semibold text-grey-200">
+        {title}
+        <InfoIcon />
+      </h4>
+      <h2 className="flex items-center gap-1 font-semibold text-grey-300">
+        {value}
+
+        <ArrowIcon variation={percentageValue} />
+
+        <span
+          className={cn("text-[1rem] leading-1 text-green-100", {
+            "text-red-100": percentage < 1,
+          })}
+        >{`${percentage < 1 ? percentage : `+${percentage}`}.0%`}</span>
+      </h2>
+    </article>
+  );
+}
